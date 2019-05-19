fb99beba255b4904c63512752ea3595a3fcc84ab
greggu <gguranic@umass.edu>
2019-05-19 18:20:02 -0400

    path bug

--- R/paths_default.R
+++ R/paths_default.R
@@ -3,6 +3,6 @@
 #' @return installed package default data paths
 default_data_set_path = function() {
   pp = system.file(package = 'dhsdata', mustWork=TRUE)
-  dp = file.path(pp, 'data')
+  dp = file.path(pp, 'default_data')
   dp
 }
