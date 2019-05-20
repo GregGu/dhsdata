7de949099a3a29bf6af8a13c6d7dc4493c6562a8
greggu <gguranic@umass.edu>
2019-05-20 15:33:59 -0400

    commit following ignore lifted

--- R/paths_relative_read.R
+++ R/paths_relative_read.R
@@ -5,3 +5,27 @@ get_recode<- function(
   data <- readRDS(path)
   data
 }
+
+get_df1<- function(
+  data_set_path = default_data_set_path()
+) {
+  path <- file.path(data_set_path, "df1.rds")
+  data <- readRDS(path)
+  data
+}
+
+get_df2<- function(
+  data_set_path = default_data_set_path()
+) {
+  path <- file.path(data_set_path, "df2.rds")
+  data <- readRDS(path)
+  data
+}
+
+get_df3<- function(
+  data_set_path = default_data_set_path()
+) {
+  path <- file.path(data_set_path, "df3.rds")
+  data <- readRDS(path)
+  data
+}
